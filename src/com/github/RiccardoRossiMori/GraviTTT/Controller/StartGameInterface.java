66508aed773039c6d0757a52eabd5527cdeca76b
Riccardo <riccardo.rossimori@studenti.unicam.it>
2020-02-13 20:57:58 +0100

    [Add] Method
    added a default method to make it extendable for checkerboards with different parameters.

--- src/com/github/RiccardoRossiMori/GraviTTT/Controller/StartGameInterface.java
+++ src/com/github/RiccardoRossiMori/GraviTTT/Controller/StartGameInterface.java
@@ -2,6 +2,7 @@ package com.github.RiccardoRossiMori.GraviTTT.Controller; /**
  * <b>Responsabilità: </b>Inizializzare una partita tra due <code>com.github.RiccardoRossiMori.GraviTTT.Model.Player</code> con una <code>com.github.RiccardoRossiMori.GraviTTT.Model.Checkerboard</code>.
  */
 
+import com.github.RiccardoRossiMori.GraviTTT.Model.CheckerboardVariables;
 import com.github.RiccardoRossiMori.GraviTTT.Model.Player;
 
 import java.io.IOException;
@@ -12,8 +13,9 @@ import java.io.IOException;
  */
 public interface StartGameInterface {
 
-    void setSize(int i, int j, boolean flag);
-
+    default <T extends CheckerboardVariables> CheckerboardVariables setSize(int i, int j, boolean flag) {//TODO chiedi a qualcuno se può andare scritto così
+        return new CheckerboardVariables(i, j, flag);//metodo marginale, mai utilizzato, ma comunque implementato per estensioni future
+    }
     void init() throws IOException;
 
     Player scegliGiocatori1(String giocatorenumerox) throws IOException;

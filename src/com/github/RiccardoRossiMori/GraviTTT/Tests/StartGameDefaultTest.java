973e0f003055f996694c0dcebe31480a02069251
Riccardo <riccardo.rossimori@studenti.unicam.it>
2020-02-26 19:42:39 +0100

    [Improving] tests
    I've modified some things to make it work better. I didn't know how to write testing code before... So, probably it's written awfully.

--- src/com/github/RiccardoRossiMori/GraviTTT/Tests/StartGameDefaultTest.java
+++ src/com/github/RiccardoRossiMori/GraviTTT/Tests/StartGameDefaultTest.java
@@ -1,32 +1,51 @@
 package com.github.RiccardoRossiMori.GraviTTT.Tests;
 
+import com.github.RiccardoRossiMori.GraviTTT.Controller.GameManager;
 import com.github.RiccardoRossiMori.GraviTTT.Controller.StartGameDefault;
 import com.github.RiccardoRossiMori.GraviTTT.Controller.StartGameInterface;
+import com.github.RiccardoRossiMori.GraviTTT.Model.*;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class StartGameDefaultTest{
+class StartGameDefaultTest {
+    CheckerboardVariables dimensioni = CheckerboardVariables.DEFAULT_SIZE;
+    GameManager game;
+    StartGameInterface startGameInterface;
 
-    @Test
-    void init() {
+    @BeforeEach
+    void setUp() {
+        this.game = new GameManager(new MatrixCheckerboard(dimensioni));
+        this.startGameInterface = new StartGameDefault();
     }
 
     @Test
     void scegliGiocatori() throws IOException {
-        StartGameInterface pippo =new StartGameDefault();
+        startGameInterface.init(game);
+        Player player;
         InputStream sysInBackup = System.in; // backup System.in to restore it later
-        ByteArrayInputStream in = new ByteArrayInputStream("bot".getBytes());
-        System.setIn(in);
+        this.giocatore(true);
+        player = startGameInterface.scegliGiocatori("prova");
+        assertEquals(player.getClass(), RandomPlayer.class);
+        this.giocatore(false);
+        player = startGameInterface.scegliGiocatori("prova2");
+        assertEquals(player.getClass(), InteractivePlayer.class);
+    }
 
-// do your thing
-
-// optionally, reset System.in to its original
-        System.setIn(sysInBackup);
-        pippo.scegliGiocatori("prova ");
+    private void giocatore(boolean uno) {
+        if (uno) {
+            String input = "bot";
+            InputStream in = new ByteArrayInputStream(input.getBytes());
+            System.setIn(in);
+        } else {
+            String input = "\n";
+            InputStream in = new ByteArrayInputStream(input.getBytes());
+            System.setIn(in);
+        }
     }
 }
